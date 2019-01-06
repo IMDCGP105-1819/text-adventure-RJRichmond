8ad3565fce629d7d4f602c93e39cb969a4806411
Rhys <rhysrichmond1@gmail.com>
2019-01-06 00:25:06 +0000

    I have continued commenting I have finished commentin g the input checker, I also cleaned up a few prints and the movement inputting as it was the only section which allowed for the words to be swapped and it wasn't consistent so I changed it. I am now going to comment through the functions

--- TextBasedGame.py
+++ TextBasedGame.py
@@ -27,63 +27,91 @@ def InputCheck(Input):
 # Here I am checking word1 (The first word in user inputted) against the avaliable commands list and then if it in equal to a word in that list. It will go through a check to see which command it is equal to.
                 if word1 == "move":
                     word2 = (str(input("Enter a direction : "))).lower()
-# If the first word is equal to move then it gets a direction (a second word) stored in word2.
-                    Move(word1,word2)                                               # Starts the move function taking both word variables.
+# If the first word is equal to move then it gets a direction (a second word) stored in word2, and then performs the move function taking both word variables.
+                    Move(word1,word2)
 
                     break                                                           # Everytime I am using break it is stopping the loop.
                 elif word1 == "check":
+# If the word which is inputted is equal to "check", since the user hasn't inputted any specific object it checks the room by default. Printing the CurrentRoom's description.
                     print (CurrentRoom)
-                    print ("To check something specific please use : Check [what you would like to check]")
+                    print ("To check something specific please use : Check [what you would like to check] , You can also checking your inventory using : Check inv")
                     break
                 elif word1 == "use":
+# If the first word is equal to "use, it then asks for a second word so what do you want to use and then if the word which they inputted is in the player inventory (Itemcheck), it will then perform the function "ItemUsing".
                     word2 = (str(input("Enter what you would like to use : "))).lower()
                     if word2 in Character.ItemCheck():
 
                         ItemUsing(word2);
                         break
-                    else:
+                    else:                                                           # If the item isn't in the character inventory list then it will respond with that you dont have the item
                         print("You don't have this item!")
                         break
+
+                elif word1 == "pickup":
+# If the word is equal to "pickup" it then takes the second word (what you want you pickup), and then does an if statement to check if the item is in the room.
+# It then the loops through the items in the room to find the item which matches the word. Once the item has been found it appends it to the character inventory list
+# then it prints telling the player they have picked it up and removes it from the rooms item list.
+                    word2 = (str(input("Enter what you would like to pickup:"))).lower()
+                    if word2 in CurrentRoom.ItemsInRoom():
+                        for i in CurrentRoom.items:
+                            if (word2 == str(i.name)):
+
+                                Character.itemsHeld.append(i)
+                                if (i.name == "picture"):
+                                    CurrentRoom.description = PorchDescription2     # If the item picked up is equal to picture or the key it changes the description to tell the player they have picked it up when they check or reenter the room.
+                                if (i.name == "key"):
+                                    CurrentRoom.description = StudyDescription2
+                                print("You have picked up a " + i.name)
+                                CurrentRoom.items.remove(i)
+                                return
+
+                    else:
+                        print("You cannot pick this up!")
+                        break
                 elif word1 == "drop":
+# if the word is equal to drop then it then asks for a second word being what do you want to drop, it then goes through the same routine of checking the item and then runs the item drop function
                     word2 = Input(str("What do you want to drop?"))
                     if word2 in Character.ItemCheck():
 
                         ItemDrop(word2);
                         break
                 elif word1 == "quit":
+# if the word is equal to quit then it runs the game end function to stop the loop.
 
                     GameEnding();
                     break
                 elif word1 == "help":
+# finally if the word is equal to help then it prints the avaliable commands list.
                     print("You can enter these commands : ",AvaliableCommands)
                     break
             else:
-                print("Unknown Command")
+                print("Unknown Command")                                            # if the word is not in the command list it print thats its unknown
 
         elif (len(Command) == 2):
+# This is now checking if the user inputs 2 words this works the same as the one word section however it doesn't ask for the second word when needed and allows for some more specific things.
             word1 = (Command[0])
             word2 = (Command[1])
-            if (word1 in AvaliableCommands) or (word2 in AvaliableCommands):
-                if (word1 == "move") or (word2 == "move"):
-                    Move(word1,word2)
+# This now stores both words rather then just the one.
+            if (word1 in AvaliableCommands):        # Does the same check to make sure word 1 is an avaliable command.
+                if (word1 == "move"):
+                    Move(word1,word2)               # Does the same as moving with one word.
                     break
                 elif word1 == "check":
+# Here it allows more much more things to be checked so if the word is equal to room it prints the room description.
                     if word2 == "room":
                         print(CurrentRoom)
                         break
                     elif word2 == "inv":
-
-                        print(Character)
+                        print(Character)            # If the word is equal to inv it prints the players current inventory
                         break
                     elif word2 in Character.ItemCheck():
-
-                        ItemCheck(word2);
+                        ItemCheck(word2);           # Or if the word is in the characters current inventory it then performs the item check function.
                         break
                     else:
-                        print("You cannot check this!")
+                        print("You cannot check this!") # Lastly if the word doesn't equal any of these then it tells the player they can't check what they are trying to.
                         break
 
-                elif word1 == "pickup":
+                elif word1 == "pickup":             # This does the same as checking an item with the one word input but doesn't need to take in another word.
                     if word2 in CurrentRoom.ItemsInRoom():
                         for i in CurrentRoom.items:
 
@@ -101,7 +129,7 @@ def InputCheck(Input):
                     else:
                         print("You cannot pick this up!")
                         break
-                elif word1 == "use":
+                elif word1 == "use":                # This is the same as using an item with the one word input, again not needing to take in another word.
                     if word2 in Character.ItemCheck():
 
                         ItemUsing(word2);
@@ -110,32 +138,37 @@ def InputCheck(Input):
                         print("You don't have this item!")
                         break
 
-                elif word1 == "drop":
+                elif word1 == "drop":               # This is the same as using an item with the one word input.
                     if word2 in Character.ItemCheck():
 
                         ItemDrop(word2);
                         break
-                elif (word1 == "quit") or (word2 == "quit"):
+                elif (word1 == "quit"):             # Finally this works exactly the same as quiting using one word, as the second word is not needed.
                     GameEnding();
 
-            else:
+            else:                                   # If the word is not in the avaliable commands then it will print saying that its an unknown command
                 print("Unknown Command")
                 break
         else:
             print("Please only enter 1 - 2 words. Enter help to see avaliable commands")
+            break
+# With the input system I decided I would take in the two different kinds of inputs being one word and two words. As I thought that it would allow the player to type in commands in the way they deemed were most comfortable.
+# Thats why some of the sections repeat.
+
+#Functions
 
 def Move(word1,word2):
     Directions = ["north","n","east","e","south","s","west","w"]
     CurrentRoom = TheRoom(Character.positionx,Character.positiony)
-    if (word1 in Directions) or (word2 in Directions):
-        if (word1 == "north" or word1 == "n") or (word2 == "north" or word2 == "n"):
+    if (word2 in Directions):
+        if (word2 == "north" or word2 == "n"):
              Character.positiony += 1
 
-        if (word1 == "east" or word1 == "e") or (word2 == "east" or word2 == "e"):
+        if (word2 == "east" or word2 == "e"):
             Character.positionx += 1
-        if (word1 == "south" or word1 == "s") or (word2 == "south" or word2 == "s"):
+        if (word2 == "south" or word2 == "s"):
             Character.positiony -= 1
-        if (word1 == "west" or word1 == "w") or (word2 == "west" or word2 == "w"):
+        if (word2 == "west" or word2 == "w"):
             Character.positionx -= 1
         CharacterPosition = [Character.positionx,Character.positiony]
 
@@ -192,6 +225,7 @@ def ItemUsing(word2):
                 print("You have used " + i.name)
                 print(i.Use)
                 Character.itemsHeld.remove(i)
+
                 if (i.name == "picture"):
                     CurrentRoom.exits.append([-1,2])
                     CurrentRoom.description = KitchenDescription2
@@ -223,4 +257,4 @@ while True:
         print("Game Ended, Thanks for playing!")
         break
     UserInput = str(input("Please enter a command : "))
-    InputCheck(UserInput.lower())
+    InputCheck(UserInput.lower()) # All of my userinputs are handled as lowercase at it makes it simpler for checks to not make mistakes.
